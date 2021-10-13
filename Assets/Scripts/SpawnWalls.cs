663f7678f2245b6a5e86a19298d293e3a4e9da17
Devon Poon Woo <devon.poonwoo@gmail.com>
2021-10-13 00:41:42 -0400

    Added random wall spawn intervals

--- Assets/Scripts/SpawnWalls.cs
+++ Assets/Scripts/SpawnWalls.cs
@@ -16,14 +16,36 @@ public class SpawnWalls : MonoBehaviour
 
     int whichWall = 0;
 
+    float randSpawnInterval;
+
+    public bool random = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        randSpawnInterval = randNum();
         prevSpawnTime = -1;
+    }
+
+    float randNum()
+    {
+        return Random.Range(1, 26) / 10.0f;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (random == true) {
+            randomSpawns();
+        }
+        else {
+            setSpawns();
+        }
+
+
+    }
+
+    void setSpawns()
     {
         if (Time.time - prevSpawnTime >= spawnInterval) {
             if (whichWall == 0) {
@@ -36,7 +58,21 @@ public class SpawnWalls : MonoBehaviour
             }
             prevSpawnTime = Time.time;
         }
+    }
 
-
+    void randomSpawns()
+    {
+        if (Time.time - prevSpawnTime >= randSpawnInterval) {
+            if (whichWall == 0) {
+                whichWall = 1;
+                Instantiate(wall_0, new Vector3(0, 0, spawnPositionZ), Quaternion.identity);
+            }
+            else {
+                whichWall = 0;
+                Instantiate(wall_1, new Vector3(0, 0, spawnPositionZ), Quaternion.identity);
+            }
+            randSpawnInterval = randNum();
+            prevSpawnTime = Time.time;
+        }
     }
 }
